852f5d06b0794ec3ba2fe90f44145e3623d1cbf8
ashin000 <ashinpramod010@gmail.com>
2023-10-06 00:29:28 +0530

    initial commit

---
+++ bin/largestNum.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<int> list = [1, -3, 7, 9, 0, -6, 4, -2, 0, 10, -8, 5];
+  int largest = 0;
+  for (int i=0;i<list.length;i++) {
+    if (list[i] > largest) {
+      largest = list[i];
+    }
+  }
+  print("largest number is: $largest");
+}
